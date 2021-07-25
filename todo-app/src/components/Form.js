52ae0cbddc5394fd82038cd32fc96c3620edbf4b
kaki-m <dskk0921@icloud.com>
2021-07-25 13:25:11 +0900

    [update] serchcomponets into index.js

---
+++ todo-app/src/components/Form.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function Form(props) {
+  const [name, setName] = useState('');
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName("");
+  }
+
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          Todo List
+        </label>
+      </h2>
+
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        追加
+      </button>
+    </form>
+  );
+}
+
+export default Form;
